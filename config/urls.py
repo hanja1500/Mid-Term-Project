fc100671482259843a55dd9abbcc1daf57d3f85d
johncreepto <johncreepto@gmail.com>
2023-05-01 23:03:19 +0900

    connect xss with django

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test', views.first_view, name='first_test')
     path('xss/', views.xss, name='xss_test'),
 ]
--- config/views.py
+++ config/views.py
@@ -1,3 +1,7 @@
 from django.http import HttpResponse
+
+def first_view(request):
+    return HttpResponse("Hello! Start of Django!")
+
 def xss(request):
     return render(request, 'xss.html')
